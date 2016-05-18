67ad9946854e936abfc8e258cce85dd4a534757d
Rodrigo Ferreira <rodrigo@rjgf.com.br>
2016-05-18 19:54:18 -0300

    - Ampliação da ação da pesquisa 	Verificando o termo pesquisado em mais de um campo da tabela

--- fnc/pesquisar.php
+++ fnc/pesquisar.php
@@ -5,7 +5,8 @@ if($_POST['busca']!=''){
     $conexao = conexaoDB();
     $sql = "SELECT pag_botao, pag_titulo, pag_chamada
             FROM paginas
-            WHERE pag_conteudo_html LIKE :busca";
+            WHERE pag_conteudo_html LIKE :busca
+            OR pag_chamada LIKE :busca";
     $stmt = $conexao->prepare($sql);
     $stmt->bindValue('busca',"%$_POST[busca]%");
     $stmt->execute();
